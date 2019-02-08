8f854a638c2db06c35a277e61ab5d5911247b8bc
Zack Moore <zmoore@usgs.gov>
2019-02-07 20:21:24 -0600

    Made initial site number more configurable to stop subsequent tests from stepping on eachother.

--- tests/integrations/load/test_plan.sh
+++ tests/integrations/load/test_plan.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 SERVER_COUNT=3
+SITES_ADDED_PER_USER=1111
+STARTING_SITE_NUMBER=100000000
 DATA_DIR="${DATA_DIR:-$(pwd)/tests/data}"
 OUTPUT_DIR="${OUTPUT_DIR:-$(pwd)/tests/output}"
 TESTS_DIR="${TESTS_DIR:-$(pwd)/tests/integrations}"
@@ -9,7 +11,7 @@ DOCKER_NETWORK_NAME="${DOCKER_NETWORK_NAME:-mlr-it-net}"
 execute_load_test() {
   LOAD_TEST_USERS_PER_SERVER=$1
   LOAD_TEST_USER_COUNT=$(($LOAD_TEST_USERS_PER_SERVER*$SERVER_COUNT))
-  echo "Starting load test with $LOAD_TEST_USERS_PER_SERVER users per server."
+  echo "Starting load test with $LOAD_TEST_USER_COUNT users (Starting Site: $STARTING_SITE_NUMBER)."
   DOCKER_OUTPUT_DIR="/tests/output/load/$LOAD_TEST_USER_COUNT"
   docker run --rm \
     --network="${DOCKER_NETWORK_NAME}" \
@@ -19,6 +21,7 @@ execute_load_test() {
     -v "${JMETER_DOCKER_DIR}/jmeter-master.properties:/jmeter/bin/user.properties" \
     -v "${JMETER_DOCKER_DIR}/config/rmi_keystore.jks:/jmeter/rmi_keystore.jks" \
     -e LOAD_TEST_USER_COUNT=$LOAD_TEST_USER_COUNT \
+    -e MIN_SITE_NUMBER=$STARTING_SITE_NUMBER \
     jmeter-base:latest jmeter \
       -f \
       -n \
@@ -29,6 +32,7 @@ execute_load_test() {
       -t /tests/integrations/load/load.jmx \
       -Rjmeter.server.1,jmeter.server.2,jmeter.server.3
   echo "Load test completed."
+  STARTING_SITE_NUMBER=$(($STARTING_SITE_NUMBER + $LOAD_TEST_USER_COUNT*$SITES_ADDED_PER_USER))
 }
 
 echo "Starting load testing."
